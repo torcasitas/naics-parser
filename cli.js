329fc451287cc48378e5764572b7a552db7034f1
odw566 <carlos.victoricaorozco@capitalone.com>
2016-09-29 11:45:53 -0700

    wrapping up things

--- cli.js
+++ cli.js
@@ -6,12 +6,6 @@ var argv = require('yargs')
         describe: 'A csv file'
     })
     .command(require('./commands/import_cmd'))
-    .command(require('./commands/export_cmd'))
-    .command(require('./commands/publish_cmd'))
-    .command(require('./commands/list_cmd'))
-    .command(require('./commands/sync_cmd'))
-    .command(require('./commands/analyze_cmd'))
-    .command(require('./commands/pagify_cmd'))
-    .global('t', 's')
+    .global('f')
     .help()
     .argv;
--- commands/import_cmd.js
+++ commands/import_cmd.js
@@ -1,20 +1,17 @@
 'use strict';
 
 exports.command = 'import'
-
 exports.describe = 'parses a csv to json for NAICS codes'
 
 exports.builder = (argv) => {
-    argv
-        .option('entry', {
-            demand: true,
-            requiresArg: true,
-            describe: 'the csv file to be parsed'
-        });
-
-    return argv;
+  argv
+    .option('entry', {
+        demand: true,
+        requiresArg: false,
+        describe: 'the csv file to be parsed'
+    });
+  return argv;
 };
-
 
 let fs = require('fs'),
     process = require('process'),
@@ -27,24 +24,73 @@ let fs = require('fs'),
 class ImportCommand {
   constructor(argv) {
     this.entry = argv.entry;
-    
-
   }
-
   
-  initialize() {
-    let dataFile = this.entry;
+  process() {
+    let data = fs.readFileSync(this.entry);
+    let parsed = Baby.parse(data.toString(), {header: false, delimiter: ','});
+    let output = [];
 
-    console.log('asfd');
-    //let data = fs.readFileSync(dataFile)
+    if (parsed.errors.length === 0) {
 
+      // col[0] - main category key
+      // col[1] - specialty/subcategory key
+      // col[2] - Main category name if specialty/subcategory key not present,
+      //        - Specialty name if specialty/subcategory key present
+      // col[3] - Sub category name 
 
+      let category;
+      let subcategory;
+      let mainCat = false;
+      let currentKey;
+
+      for (let cols of parsed.data) {
+        // console.log('row ' + cols + ' isMain ' + ((cols[0]) ? ' MAIN ' : 'specialty' ));
+        if (cols[0]) {
+          category = {};
+          category.id = cols[0];
+          category.name = cols[2].trim();
+          category.subcategories = [];
+          output.push(category); 
+
+          // let notEmptyCategory = Object.keys(category).length > 0; 
+          // if (notEmptyCategory) {
+          //   console.log('como crees');
+            
+          // } else {
+            
+          // }
+          
+        } else {
+          if (cols[3] && cols[1] !== currentKey) {  // Specialties
+            subcategory = {}
+            subcategory.id = cols[1];
+            subcategory.name = cols[3].trim();
+            subcategory.specialties = [];
+            currentKey = cols[1];
+            subcategory.specialties.push(cols[2].trim());
+            category.subcategories.push(subcategory);
+          } else {
+            // console.log('same key');
+            subcategory.specialties.push(cols[2].trim());   
+          }  
+        }
+        
+      }
+
+      fs.writeFile('output.json', JSON.stringify(output, null, 2), (err, data) => {
+        if (err) {
+          return console.log('error: ' + err);
+        }
+        console.log('Successfully written to output.json');
+      });
+      // console.log('**************');
+      // console.log(util.inspect(output, {depth:10, colors: true}));
+    }
   }
-
 };
 
 exports.handler = (argv) => {
-
   let importCmd = new ImportCommand(argv);
-  importCmd.initialize();
+  importCmd.process();
 };
